1b5312edcfa5b128be366cddf7ceb9b18af8e338
Philip Choi <grjkz0@gmail.com>
2015-08-26 12:07:34 -0400

    stf

--- validation/app/models/example.rb
+++ validation/app/models/example.rb
@@ -1,5 +1,14 @@
 class Example < ActiveRecord::Base
 
-	validates :email, presence: true
-	
+	# validates :email, presence: true
+
+	# validates :email, confirmation: true
+	# validates :email_confirmation, presence: true
+
+	# validates :name, length: { in: 2..10 }
+	# validates :email, length: { minimum: 9 } 
+		# too_short: "%{count} |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" }
+
+		validates :name, numericality: true
+
 end
