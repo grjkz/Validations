21ebef0804dd329bebfae1e72aaba7b5d5ac78ee
Viktoria Szontagh <vikkiszontagh@gmail.com>
2015-08-26 12:22:01 -0400

    update

--- validation/app/models/example.rb
+++ validation/app/models/example.rb
@@ -14,10 +14,10 @@ class Example < ActiveRecord::Base
 
 end
 
-	validates :email, presence: true
-	validates :name, presence: true, length: { minimum: 2 }, :message "can't be blank, is too short (minimum is 2 characters)"
-  example = Example.new
-  example.valid? # => false
-  example.errors.messages
-end
+# 	validates :email, presence: true
+# 	validates :name, presence: true, length: { minimum: 2 }, :message "can't be blank, is too short (minimum is 2 characters)"
+#   example = Example.new
+#   example.valid? # => false
+#   example.errors.messages
+# end
 
